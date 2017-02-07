15f50c22b96cbceb9014d120ad10275f578d83ce
FlorianSpadaro <spadaro.florian@outlook.fr>
2017-02-07 09:22:32 +0100

    Add files via upload

---
+++ getServiceComptable.php
@@ -0,0 +1,21 @@
+<?php
+	function getServiceComptableById($id)
+	{
+		include("connexionBdd.php");
+		$serviceComptable = null;
+		
+		$req = $bdd->prepare("SELECT * FROM service_comptable WHERE id = ?");
+		$req->execute(array($id));
+		if($data = $req->fetch())
+		{
+			$serviceComptable["id"] = $data["id"];
+			$serviceComptable["libelle"] = $data["libelle"];
+			$serviceComptable["description"] = $data["description"];
+			
+			include("getLieu.php");
+			$serviceComptable["lieu"] = json_decode(getLieuById($data["id_lieu"]));
+		}
+		
+		return json_encode($serviceComptable);
+	}
+?>
