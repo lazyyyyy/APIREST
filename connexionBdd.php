2e964c7aec9241cfd58c22c711078850bde4bf5a
FlorianSpadaro <spadaro.florian@outlook.fr>
2017-02-16 10:55:20 +0100

    Delete connexionBdd.php

--- connexionBdd.php
+++
@@ -1,8 +0,0 @@
-<?php
-try{
-	$bdd = new PDO('mysql:host=127.0.0.1;dbname=gsb;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-}
-catch (Exception $e){
-	die('Erreur : '.$e->getMessage());
-}
-?>
